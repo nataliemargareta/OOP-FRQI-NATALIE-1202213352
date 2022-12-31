5cae4339e9ab5df9c9e962edfdc31acc922eb31f
nataliemargareta <116381456+nataliemargareta@users.noreply.github.com>
2022-12-31 13:40:29 +0700

    Add files via upload

---
+++ App.java
@@ -0,0 +1,9 @@
+package MODUL 3;
+
+public class App {
+    public class App {
+        public static void main(String[] args) throws Exception {
+            System.out.println("Menu Program");
+        }
+    } 
+}
---
+++ Calculation.java
@@ -0,0 +1,32 @@
+package MODUL 3;
+
+public class Calculation {
+    do {
+        try{
+            switch(.....){
+                case 1:
+                break;
+                case 2:
+                break;
+                case 3:
+                break;
+                case 0:
+                default;
+
+                continue;
+            }
+            repeat = ....;
+
+ } catch (.....){
+
+ } catch (.....){
+
+ } catch (.....){
+ }
+} while (....);
+scannerName.close();
+
+            }
+        }
+    }
+}
